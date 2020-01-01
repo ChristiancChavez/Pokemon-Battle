b0ac9f3c303d7d48da46c41e99e83cb71f30ef2c
Christian Chavez <christiancchavez10@hotmail.com>
2020-01-01 13:36:27 -0500

    changed shape of SignIn form

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import Button from '../Button';
 import './LogIn.scss';
 
 class LogIn extends Component {
 
     state = {
-        username: "",
         pokename: "",
         email: "",
         password: "",
@@ -17,53 +17,48 @@ class LogIn extends Component {
         e.preventDefault();
     }
     render(){
-        const { username, pokename, email, password } = this.state;
+        const { pokename, email, password } = this.state;
         return(
             <article className="popup">
-                
-                <form className="form" >
-                    <h3>SIGNUP</h3>
-                    <span onClick={this.toggleButtonPopup}>X</span>
-                    <input
-                        name="username" 
-                        placeholder="Name" 
-                        value={username} 
-                        type="text"  autoFocus  
-                        pattern="[a-z]{2,15}" 
-                        title="Your username must be 2 to 15 in length"
-                        required
-                        onChange={this.onChange}  
-                    />
-                    <input
-                        name="pokename" 
-                        placeholder="PokeName"
-                        value={pokename} 
-                        type="text"  
-                        pattern="[a-z0-9]{2,15}" 
-                        title="Your username must be contain one number at least and be 8 to 12 in length" 
-                        required
-                        onChange={this.onChange} 
-                    />
-                    <button className="form__button"  type="submit" ></button>
-                    <input
-                        name="email" 
-                        placeholder="E-mail" 
-                        value={email} 
-                        type="email" 
-                        required
-                        onChange={this.onChange}  
-                    />
-                    <input
-                        name="password" 
-                        placeholder="Password" 
-                        value={password} 
-                        type="password" 
-                        pattern="[A-Za-z0-9]{8,12}" 
-                        title="Password must be 8 to 12 in length, has uppercase and lowercase letters and one number at least" 
-                        required
-                        onChange={this.onChange}  
-                    />
-                </form>
+                <div className="pokeball">
+                    <form className="form" >
+                        <h3>SIGNUP</h3>
+                        <span className="form__close" onClick={this.toggleButtonPopup}>X</span>
+                        <input
+                            name="pokename" 
+                            placeholder="PokeName"
+                            value={pokename} 
+                            type="text"  
+                            pattern="[a-z0-9]{2,15}" 
+                            title="Your username must be contain one number at least and be 8 to 12 in length" 
+                            required
+                            onChange={this.onChange} 
+                        />
+                        <input
+                            name="email" 
+                            placeholder="E-mail" 
+                            value={email} 
+                            type="email" 
+                            required
+                            onChange={this.onChange}  
+                        />
+                        <input
+                            name="password" 
+                            placeholder="Password" 
+                            value={password} 
+                            type="password" 
+                            pattern="[A-Za-z0-9]{8,12}" 
+                            title="Password must be 8 to 12 in length, has uppercase and lowercase letters and one number at least" 
+                            required
+                            onChange={this.onChange}  
+                        />
+                        <button className="form__button"  type="submit" >Confirm</button>
+                        <div className="form__social">
+                            <Button text="Sign in Facebook" classBtn="facebook"/>
+                            <Button text="Sign in Google " classBtn="google"/>
+                        </div>
+                    </form>
+                </div>
             </article>
         )
     }
