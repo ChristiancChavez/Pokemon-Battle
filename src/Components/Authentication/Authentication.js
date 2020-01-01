a9c91c9a517593b9b4f76c1766404c318aeb3521
Christian Chavez <christiancchavez10@hotmail.com>
2020-01-01 17:35:58 -0500

    add Authentication component

---
+++ src/Components/Authentication/Authentication.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+//Components
+import Input from '../Input';
+
+class Authentication extends Component {
+
+    render(){
+        return (
+            <div className="authentication">
+                <form>
+                    
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default Authentication;
---
+++ src/Components/Input/Input.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const Input = ({ name }) => (
+    <Fragment>
+        <label>{name}</label>
+        <input
+            type="text"  
+            // pattern="[a-z0-9]{2,15}" 
+            // title="Your username must be contain one number at least and be 8 to 12 in length" 
+            required
+            onChange={this.onChange} 
+        />
+    </Fragment>
+)
+
+export default Input;
