49910b534909620a881bf5baf1bec7e9da11c75c
Christian Chavez <christiancchavez10@hotmail.com>
2020-02-10 14:33:22 -0500

    create login functionality of form component

--- src/Components/Authentication/Authentication.js
+++ src/Components/Authentication/Authentication.js
@@ -5,18 +5,45 @@ import Button from '../Button';
 //styles
 import './Authentication.scss';
 
-const Authentication = ({ identification, handleCloseIdentification } ) => {
+const Authentication = ({ handleChange, identification, handleCloseIdentification, addUserAuthenticationDataBase, getUserAuthenticationDataBase, password, pokename, email })=> {
+   
     const userSignUp = identification === 'Signup'? false : true;
     return (
         <div className="authentication">
             <div className="authentication__pokeball">
-                <form className="form">
+                <form className="form" onSubmit={ event => addUserAuthenticationDataBase(event)}>
                     <h3>{identification}</h3>
                     <Button classBtn="close-authentication" onClick={handleCloseIdentification}>X</Button>
-                    <Input label='Pokename' />
-                    {userSignUp && <Input label='E-mail'/>}
-                    <Input label='Password'/>
-                    <Button classBtn="yellow" onClick={handleCloseIdentification}>{identification}</Button>
+                    <Input 
+                        label='Pokename' 
+                        name='pokename'
+                        type='text' 
+                        pattern='[a-z0-9]{4,6}'
+                        value={pokename}
+                        title='Your username must be contain one number at least and be 4 to 6 in length' 
+                        onChange={event => handleChange('pokename', event.target.value)}
+                    />
+                    {userSignUp && 
+                        <Input 
+                            label='E-mail' 
+                            type='email'
+                            name='email' 
+                            value={email}
+                            title='Your e-mail must be in the following order: characters@characters.domain'
+                            pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$' 
+                            onChange={event => handleChange('email', event.target.value)}
+                        />
+                    }
+                    <Input 
+                        label='Password' 
+                        type='password' 
+                        name='password'
+                        value={password} 
+                        pattern="[a-z0-9]{8,15}"
+                        title='Your password must be contain one number at least and be 8 to 15 in length' 
+                        onChange={event => handleChange('password', event.target.value)}
+                    />
+                    <Button classBtn="yellow"  type="submit">{identification}</Button>
                     <div className="form__social">
                         <Button classBtn="facebook">Facebook</Button>
                         <Button classBtn="google">Google</Button>
@@ -25,6 +52,7 @@ const Authentication = ({ identification, handleCloseIdentification } ) => {
             </div>
         </div>
     )
-}
+}  
 
 export default Authentication;
+
--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -18,25 +18,63 @@ class Home extends Component {
     state = {
         identification: '',
         showAuthentication: false,
+        idBtnIdentification: '',
+        users: [],
+        pokename: '',
+        email: '',
+        passwoord: '',
+        getUser: {}
     }
+    
+    handleChange = (name, value) => {
+        this.setState({
+            [name]: value
+        })
+    };
 
     handleIndentification = (title) => {
         this.setState({
             identification: title,
             showAuthentication: true,
         })
-        console.log(title);
     };
 
     handleCloseIdentification = () => {
         this.setState({
             showAuthentication: false,
         })
-    }
+    };
+
+    addUserAuthenticationDataBase = (event) => {
+        const { email, password, pokename } = this.state;
+
+        const user = {
+            pokename,
+            password,
+            email
+        };
+        
+        this.setState({
+            users: [...this.state.users, user],
+        })
+        // this.handleCloseIdentification();
+        event.preventDefault();
+    };
+
+    getUserAuthenticationDataBase = (event) => {
+        const { users } = this.state;
+        const userGetted = users.filter((user) => user)
+        this.setState({
+            getUser: userGetted,
+        }) 
+        event.preventDefault();
+    };
+    
+
     render() { 
         const { location:{ pathname } } = this.props;
-        const { showAuthentication, identification } = this.state;
-
+        const { showAuthentication, identification, idBtnIdentification, pokename, password, email  } = this.state;
+        console.log(this.state.pokename,this.state.password,this.state.email,this.state.users);
         return (         
             <section className="home">
                 <Header 
@@ -47,6 +85,13 @@ class Home extends Component {
                     <Authentication 
                         identification={identification} 
                         handleCloseIdentification={this.handleCloseIdentification} 
+                        addUserAuthenticationDataBase= {this.addUserAuthenticationDataBase}
+                        getUserAuthenticationDataBase={this.getUserAuthenticationDataBase}
+                        handleChange={this.handleChange}
+                        pokename={pokename}
+                        password={password}
+                        email={email}
+                        idBtnIdentification={idBtnIdentification}
                     />
                 }
             </section>
